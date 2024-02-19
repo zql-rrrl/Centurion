260b96f989f0b4daa8eea8ca7f0aec57350af183
rrrl <zql_rrrl@163.com>
2024-02-20 01:36:36 +0800

    test home

--- Centurion/urls.py
+++ Centurion/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
     path('', include('quantum_fluctuations.urls'))
 ]
